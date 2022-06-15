7bdd1dd24d25b0e28fb059ae739138b55e350099
Jakub Maciasz <maciasz.jakub@gmail.com>
2022-06-15 18:37:13 +0200

    Initial commit

---
+++ base/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from ckeditor.fields import RichTextField
+from ckeditor_uploader.fields import  RichTextUploadingField
+# Create your models here.
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+class Post(models.Model):
+    headline = models.CharField(max_length=200)
+    sub_headline = models.CharField(max_length=200, null=True, blank=True)
+    thumbnail = models.ImageField(null=True, blank=True, upload_to="images", default="placeholder.png")
+    body = RichTextUploadingField(null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    active = models.BooleanField(default=False)
+    featured = models.BooleanField(default=False)
+    tags = models.ManyToManyField(Tag, null=True)

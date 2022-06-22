2cd997ca5915bc0c02834ea9202741ede552ea67
Jakub Maciasz <maciasz.jakub@gmail.com>
2022-06-22 17:34:38 +0200

    cv, github update

--- static/js/script.js
+++ static/js/script.js
@@ -38,3 +38,20 @@ function setTheme(mode){
 
 	localStorage.setItem('theme', mode)
 }
+
+let toggleBtn = document.getElementById("toggleBtn");
+let info = document.getElementById("info");
+
+let toggle = false;
+
+toggleBtn.addEventListener("click", () => {
+    if (toggle) {
+        info.style = 'transform: translateY(8rem);';
+        toggleBtn.innerHTML = '<i class="fas fa-arrow-up"></i>'
+        toggle = false;
+    }   else {
+        info.style = 'transform: translateY(0rem);';
+        toggleBtn.innerHTML = '<i class="fas fa-arrow-down"></i>'
+        toggle = true;
+        }
+       })
